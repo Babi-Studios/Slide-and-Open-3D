7541e901e88df2cc6352ca66c380a44f191ed719
aeris170 <aeris170@gmail.com>
2020-06-26 12:53:52 +0300

    Fix hatches being unable to open after losing the game and restarting

---
+++ Assets/OnSceneLoad.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnSceneLoad : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene aScene, LoadSceneMode aMode)
+    {
+        Hatch.AllHatchesAreClosed = true;
+    }
+}
